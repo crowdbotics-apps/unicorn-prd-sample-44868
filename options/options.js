ded121e9eeb7c9e06c967ba51a0312f196c5a7f5
Crowdbotics <team@crowdbotics.com>
2023-12-05 17:04:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "unicorn-prd-sample-44868",
+  url: "https://unicorn-prd-sample-44868.botics.co",
+  api: "https://unicorn-prd-sample-44868.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
